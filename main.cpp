deec4d8ec82f511ee8ad611602698cf7f46ea7a4
jfd84 <jfd84@georgetown.edu>
2022-10-06 12:01:28 -0400

    Start

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Joe D'Agostino on 10/6/22.
+//
+
+#include "main.h"
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cout << "Hello World" << endl;
+    return 0;
+}
